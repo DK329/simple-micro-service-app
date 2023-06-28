f6649c651328cbf654ec43cd6d879e1c49abca2c
tharuka-dilshan <tharukadk329@gmail.com>
2023-06-28 17:12:56 +0530

    almost done front end

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Book} from "./dto/book";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,58 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front-end';
+
+  bookList:Array<Book> =[];
+  http:HttpClient;
+
+  constructor(http:HttpClient) {
+    this.http=http;
+    http.get<Array<Book>>('http://localhost:8080/api/v1/books').subscribe(bookList=>{
+      this.bookList=bookList;
+    })
+  }
+
+  addBook(isbn: HTMLInputElement, title: HTMLInputElement) {
+    if (!isbn.value.trim()) {
+      isbn.select();
+      return;
+    }
+    if (!title.value.trim()) {
+      title.select();
+      return;
+    }
+    this.http.post<Book>('http://localhost:8080/api/v1/books', new Book(isbn.value, title.value))
+      .subscribe(book => {
+        this.bookList.push(book);
+        isbn.value = '';
+        title.value = '';
+        isbn.focus();
+      })
+  }
+  deleteBook(book: Book) {
+    this.http.delete(`http://localhost:8080/api/v1/books/${book.isbn}`).subscribe(data => {
+      const index: number = this.bookList.indexOf(book);
+      this.bookList.splice(index, 1)
+    })
+
+  }
+
+  updateBook(isbn: HTMLInputElement, title: HTMLInputElement) {
+    if (!isbn.value.trim()) {
+      isbn.select();
+      return;
+    }
+    if (!title.value.trim()) {
+      title.select();
+      return;
+    }
+    this.http.patch<Book>(`http://localhost:8080/api/v1/books/${isbn.value}`, new Book(isbn.value, title.value))
+      .subscribe(book => {
+        book.isbn=isbn.value;
+        book.title=title.value;
+        isbn.value = '';
+        title.value = '';
+        isbn.focus();
+      })
+  }
 }
